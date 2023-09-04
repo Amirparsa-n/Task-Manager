0437cb4a4c291c1060007611e4facb85775c83ce
Amirparsa Naghibi <amirparsanaghibi@gmail.com>
2023-09-04 12:23:39 +0330

    Added fonts
    Create folder structure

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,38 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: 'class',
   content: [
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: '1rem',
+        sm: '2rem',
+        lg: '2rem',
+        xl: '2rem',
+        '2xl': '2rem',
+      },
+      // padding: '2rem',
+
+    },
+
+    fontFamily: {
+      Mazzard: ['Mazzard H'],
+    },
+
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        primary: '#3E7BFA',
+      },
+
+      boxShadow: {
+        'button': '0 3px 15px -1px rgba(233, 80, 81, 0.6)',
+        'MobileNavigationBar': '0 0 15px 1px rgba(0, 0, 0, 0.22) ',
+        'simple': '0 0 15px -2px rgba(0, 0, 0, 0.22)'
       },
     },
   },
