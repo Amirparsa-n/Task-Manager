4e4d5d6b353d4d2b601fdd9911244fb21fc4e056
Amirparsa Naghibi <amirparsanaghibi@gmail.com>
2023-09-19 11:43:12 +0330

    Add api for add task to project

---
+++ pages/api/project/[projectTaskId].js
@@ -0,0 +1,95 @@
+import User from "@/models/User";
+import connectDB from "@/utils/connectDB";
+import Joi from "joi";
+import { authOptions } from "../auth/[...nextauth]";
+import { getServerSession } from "next-auth";
+
+export default async function handler(req, res) {
+    try {
+        await connectDB();
+    } catch (err) {
+        console.log(err);
+        return res
+            .status(500)
+            .json({ status: "failed", message: "Error in connecting to DB" });
+    }
+
+    const session = await getServerSession(req, res, authOptions);
+    if (!session) {
+        return res
+            .status(401)
+            .json({ status: "failed", message: "You are note logged in!" });
+    }
+
+    const user = await User.findOne({ email: session.user.email });
+    if (!user) {
+        return res
+            .status(404)
+            .json({ status: "failed", message: "user not found" });
+    }
+
+    const deleteSchema = Joi.string().required();
+
+    const taskProjectSchema = Joi.object({
+        title: Joi.string().required().max(80),
+        description: Joi.string().optional().allow("").max(200),
+        status: Joi.string().required(),
+        tag: Joi.string().optional().allow("").max(10),
+        rating: Joi.string().required(),
+        date: Joi.date().required(),
+    });
+
+    if (req.method === "POST") {
+        const { projectTaskId } = req.query;
+        const projectName = deleteSchema.validate(projectTaskId);
+
+        const taskProject = taskProjectSchema.validate(req.body, {
+            abortEarly: true,
+        });
+
+        if (projectName.error) {
+            console.log(projectName.error);
+            const { message } = projectName.error.details[0];
+            return res.status(422).json({ status: "failed", message });
+        }
+
+        if (taskProject.error) {
+            console.log(taskProject.error);
+            const { message } = taskProject.error.details[0];
+            return res.status(422).json({ status: "failed", message });
+        }
+
+        try {
+            const filteredProjects = await User.find(
+                {
+                    "project.name": projectName.value,
+                },
+                {
+                    "project.$": 1,
+                }
+            );
+            const [project] = filteredProjects.map((user) => user.project[0]);
+            if (!project) {
+                return res
+                    .status(404)
+                    .json({ status: "failed", message: "Project notFound!" });
+            }
+
+            const projectIndex = user.project.findIndex(item => item.name === project.name);
+
+            user.project[projectIndex].todos.push(taskProject.value);
+            user.save();
+
+            res.status(201).json({
+                status: "success",
+                message: "Task project created!",
+            });
+        } catch (err) {
+            console.log(err);
+            res.status(500).json({
+                status: "failed",
+                message: "Error creating task project",
+            });
+        }
+    }
+}
